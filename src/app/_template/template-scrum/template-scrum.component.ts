1ebe6eebc3968891938de2e1ae0dcc4d9f251fc0
Frohkodil <siebos12@gmail.com>
2021-03-14 19:26:35 +0100

    Connected to backend

--- src/app/_template/template-scrum/template-scrum.component.ts
+++ src/app/_template/template-scrum/template-scrum.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StickyNoteComponent} from '../sticky-note/sticky-note.component';
 import {Data} from '../../_service/data';
+import {DataService} from '../../_service/data.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-template-scrum',
@@ -9,20 +11,33 @@ import {Data} from '../../_service/data';
 })
 export class TemplateScrumComponent implements OnInit {
   data: Data;
-  notes: Data[] = [];
+  public notes: Data[] = [];
 
-  constructor() {
+  constructor(private dataService: DataService) {
     this.data = new Data('hi', 'test');
     this.notes.push(this.data);
   }
 
   ngOnInit(): void {
+    this.getTasks();
+  }
+
+  public getTasks(): void {
+    this.dataService.getTasks().subscribe(
+      (response: Data[]) => {
+        this.notes = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
   }
 
   onSubmit(data: Data): void {
     this.data = data;
     this.data.date = new Date();
     this.notes.push(this.data);
+    this.dataService.addNote(data);
   }
 }
 
